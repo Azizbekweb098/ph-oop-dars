95a5a1160dd8bd5e188b37ff95ae90275ea11cc8
Azizbek <webcoderazizbek@gmail.com>
2024-09-20 16:17:24 +0500

    oop-dars

---
+++ class-object-1-php.php
@@ -0,0 +1,80 @@
+<?php
+
+//class Car
+//{
+//    public $name = 'Supra';
+//    public $year = 2008;
+//    public $price = '30000$';
+//    public $color = 'blakc';
+//
+//    public function y($speed)
+//    {
+//        return 'Salom Bu moshining' . $this->name . 'Va uning yurushi tezligi' . $speed . 'Va uning ishlab chiqarilgan sanasi'. $this->year . ' Va uning narxi' . $this->price;
+//    }
+//    public function rengi($color)
+//    {
+//        return $this->$color = $color;
+//    }
+//}
+//
+//$Supra = new Car();
+//echo $Supra->y('200km/s');
+//echo "<br>";
+//echo $Supra->rengi('red');
+
+// keyingisi
+
+//class Car_2
+//{
+//    public $name = 'Azizbek';
+//    private $name_2;
+//    protected $name_3 = 'Azizbek';
+//
+//    public function namel()
+//    {
+//        return $this->name_2 = 'Azizbek';
+//    }
+//
+//}
+
+//class ExitAzizbek extends Car_2
+//{
+//    public function boyi($sm)
+//    {
+//        return 'Salom Mening isim ' . $this->name_3 . 'va uning boyi ' . $sm;
+//    }
+//}
+//
+//$Azizbek = new ExitAzizbek();
+//echo $Azizbek->namel();
+//
+//// keyingisi
+//
+//class Flowers
+//{
+//    public $name;
+//    public $color;
+//    public $price;
+//
+//
+//}
+
+//class ExitFlowers extends Flowers
+//{
+//    public function rr($hid)
+//    {
+//        return 'Salo bu gulning nomi ' . $this->name . ' Va uning hidi' . $hid . ' narxlar bilan tanishsak ' . $this->price;
+//    }
+//    public function __construct($name, $color, $price)
+//    {
+//        $this->name = $name;
+//        $this->color = $color;
+//        $this->price = $price;    b
+//    }
+//}
+//Bb
+//
+//$Atirgul = new ExitFlowers('AtirGul', 'Red', '10$');
+//var_dump($Atirgul);
+
+// keyingisi
